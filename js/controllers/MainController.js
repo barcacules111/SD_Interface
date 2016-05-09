36ce9f67cb5894b0579b642f4a8def24219a0927
barcacules111 <barcacules111@gmail.com>
2016-05-09 15:51:39 +0300

    controller
    Пустые функции! написать ф без исп деректив для ( upload , save )

---
+++ js/controllers/MainController.js
@@ -0,0 +1,14 @@
+app.controller('MainController', ['$scope', function($scope)
+{
+  $scope.upload1 = function(index) { };
+   $scope.upload2 = function(index) { }; 
+    $scope.action3 = function(index) { };
+    $scope.action4 = function(index) { };
+    
+    
+    $scope.add = function(index) { }; 
+    $scope.edit = function(index) { };
+    $scope.delete = function(index) { };
+    
+    
+}];
